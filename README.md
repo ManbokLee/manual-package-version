9cac5ca32038b433e3ff4e044654785409b517fd
이만복 <mblee@aegisdds.com>
2021-01-05 16:09:26 +0900

    update

--- README.md
+++ README.md
@@ -1,3 +1,33 @@
 # Manual package version
 
-## Test...
+> When you use the package, you choose how you want to change the version.   
+The changed version will be saved according to the type you select.
+
+## Warning
+- this package is change version in package.json file. 
+
+### Supported formats version
+- format: [major version number].[minor version number].[patch version number]
+
+### Install
+```
+npm install manual-package-version
+yarn add manual-package-version
+```
+
+### How to use
+- NPX
+```
+npx manual-package-version
+```
+- scripts in package.json
+```
+{
+  "update:version": "npx manual-package-version",
+  "deploy": "yarn update:version && [your deploy scripts]"
+}
+```
+
+### Todo list
+- Support formats of different types.
+- use arguments
--- index.js
+++ index.js
@@ -1,97 +1,96 @@
-const fs = require('fs')
-const readline = require('readline')
-let jsonData = null
-let choice = null
-let version, newVersions
+#!/usr/bin/env node
+
+var fs = require('fs');
+var readline = require('readline');
+var jsonData = null;
+var choice = null;
+var version, newVersions;
 
 function init () {
   fs.readFile('package.json', 'utf8', function (err, data) {
     if (err) {
-      return
+      return;
     }
-    jsonData = JSON.parse(data)
-    version = jsonData.version
-    let pure = true
-    const versions = version.split('.').reduce((acc, cur) => {
-      const num = cur.replace(/[^\d]/g, '')
-      if (isNaN(num)) {
-        pure = false
-        return acc
+    jsonData = JSON.parse(data);
+    version = jsonData.version;
+    var pure = true;
+    var versionsArr = version.split('.');
+    var versions = [];
+    for (var i = 0; i < versionsArr.length; i++) {
+      var item = versionsArr[i];
+      if (isNaN(item)) {
+        pure = false;
+        continue;
       }
-      acc.push(num)
-      return acc
-    }, [])
+      versions.push(item)
+    }
 
     if (versions.length !== 3 || !pure) {
-      console.warn('Diff format version')
-      return
+      console.warn('Diff format version');
+      return;
     }
     newVersions = {
-      first: `${Number(versions[0]) + 1}.${versions[1]}.${versions[2]}`,
-      middle: `${versions[0]}.${Number(versions[1]) + 1}.${versions[2]}`,
-      tail: `${versions[0]}.${versions[1]}.${Number(versions[2]) + 1}`
+      first: (Number(versions[0]) + 1) + '.0.0',
+      middle: versions[0] + '.' + (Number(versions[1]) + 1) + '.0',
+      tail: versions[0] + '.' + Number(versions[1]) + '.' + (Number(versions[2]) + 1)
     }
-    viewOptions(version, newVersions)
-    inputCommand()
+    viewOptions(version, newVersions);
+    inputCommand();
   })
 }
 
 function viewOptions () {
-  console.log('Set package.json version...')
-  console.log('Now version is ' + version)
-  console.log('What you need?')
-  console.log('-------------------------------------')
-  console.log('1. Update main version => ' + newVersions.first)
-  console.log('2. Update middle version => ' + newVersions.middle)
-  console.log('3. Update tail version => ' + newVersions.tail)
-  console.log('4. None => ' + version)
-  console.log('-------------------------------------')
+  console.log('Set package.json version...');
+  console.log('Now version is ' + version);
+  console.log('What do you need?');
+  console.log('-------------------------------------');
+  console.log('1. Update major version => ' + newVersions.first);
+  console.log('2. Update minor version => ' + newVersions.middle);
+  console.log('3. Update patch version => ' + newVersions.tail);
+  console.log('4. None => ' + version);
+  console.log('-------------------------------------');
 }
 
 function inputCommand () {
-  const rl = readline.createInterface({
+  var rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout
-  })
+  });
   rl.on('line', function (line) {
-    choice = parseInt(line)
+    choice = parseInt(line);
     if (choice && choice > 0 && choice < 5) {
-      rl.close()
+      rl.close();
     } else {
-      viewOptions()
-      console.warn('Wrong choice')
-      console.warn('-------------------------------------')
+      viewOptions();
+      console.warn('Wrong choice');
+      console.warn('-------------------------------------');
     }
   }).on('close', function () {
     if (choice !== null) {
-      updateVersion()
-      process.exit()
+      updateVersion();
+      process.exit();
     }
   })
 }
 
 function updateVersion () {
   if (choice === 4) {
-    console.log('Not change version.')
-    return
+    console.log('Not change version.');
+    return;
   }
   switch (choice) {
     case 1:
-      jsonData.version = newVersions.first
+      jsonData.version = newVersions.first;
       break
     case 2:
-      jsonData.version = newVersions.middle
+      jsonData.version = newVersions.middle;
       break
     case 3:
-      jsonData.version = newVersions.tail
+      jsonData.version = newVersions.tail;
       break
   }
-  fs.writeFileSync('package.json', JSON.stringify(jsonData, null, 2))
-  console.log('Change version to ' + jsonData.version)
+  fs.writeFileSync('package.json', JSON.stringify(jsonData, null, 2));
+  console.log('Change version to ' + jsonData.version);
 }
 
-if (require.main === module) {
-  init()
-}
-
-module.exports = init
+init();
